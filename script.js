b3b02c951462dc21fe6506d053faa8dd8ce5dd8d
Ameer Hamza <76174830+ameerhamza0252@users.noreply.github.com>
2024-01-02 20:58:29 +0000

    V1.2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function menu(){
     let navHeight=document.getElementById("top");
     if(navHeight.style.height=="" ||navHeight.style.height=='0vh'){
@@ -8,3 +9,22 @@ function menu(){
         navHeight.style.height='0vh';
     }
 }
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 100;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+        reveals[i].classList.add("service-card");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  window.addEventListener("scroll", reveal);
+
+
+
