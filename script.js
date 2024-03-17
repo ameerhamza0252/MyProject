d740f5a2d5f9dd3d6463126b8bd0432d5e02e451
Ameer Hamza <76174830+ameerhamza0252@users.noreply.github.com>
2024-03-17 21:17:13 +0000

    Updating banner

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ function menu(){
 
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
-   
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
